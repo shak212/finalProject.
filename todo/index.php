8890aff2d9de1e681039dfa1469a010787b0d44d
Shakawat Bhuiyan <sb657@gmail.com>
2017-12-18 20:56:03 -0500

    hi

--- todo/index.php
+++ todo/index.php
@@ -6,13 +6,15 @@
 
 		<link href="https://fonts.googleapis.com/css?family=Raleway:400i" rel="stylesheet">
 		<link href="https://fonts.googleapis.com/css?family=Shadows+Into+Light+Two" rel="stylesheet">
-		<link rel="stylesheet" href="css/main.css">
+		<link rel="stylesheet" href="main.css">
 
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	</head>
 	<body>
 		<div class="list">
 			<h1 class="header">Welcome to your To Do List.</h1>
+
+			<a href="">Click me</a>
 
 			<ul>
 				<li><span class="item">Get food</span></li>
